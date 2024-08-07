c0934317b319622fa1f793feac7a58ae8fec1d44
ymeister <47071325+ymeister@users.noreply.github.com>
2024-08-07 07:48:03 +0400

    Update bounds with 'cabal-bounds' & clean warnings

--- src/Control/Monad/ReaderIO.hs
+++ src/Control/Monad/ReaderIO.hs
@@ -2,14 +2,12 @@
 {-# language MultiParamTypeClasses #-}
 {-# language FlexibleInstances #-}
 {-# language CPP #-}
+
 module Control.Monad.ReaderIO
-  (
-    ReaderIO (..)
-  )
-  where
+  ( ReaderIO (..)
+  ) where
 
 import Control.Monad.Fix
-import Control.Applicative
 import Control.Monad
 import Control.Monad.Reader.Class
 import Control.Monad.IO.Class
